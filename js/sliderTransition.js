f470f8b31d3dc211330a6d5baae2d5c0777615f7
Jonathan Schoonbroodt <hello@johndoesit.be>
2024-04-07 11:45:33 +0200

    init project - 30'
    write the base of the html
    import base for LESS css
    writing pseudo code basic logic for the script who's gonna handle our transition
    add pictures for the two variant of the chair

---
+++ js/sliderTransition.js
@@ -0,0 +1,10 @@
+const cardTransitionContainer = document.getElementById('card-transition-container')
+
+/* 
+  when clicking on the arrow: 
+  1. we apply the transition to the rounded container
+  2. we change the number in the currentslide span
+  3. we apply the background-color transition to the background
+  4. we apply the fade-out / fade-in transition to the chairs pictures
+*/
+
