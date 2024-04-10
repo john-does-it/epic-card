fdb685f475c8863cc85a800bdc8e3b884a0de6d4
jonathanschoonbroodt <hello@johndoesit.be>
2024-04-10 16:19:41 +0200

    next slide + tweaking animation (smoothier)

--- js/sliderTransition.js
+++ js/sliderTransition.js
@@ -1,22 +1,45 @@
 const productContainer = document.getElementById('product-container')
 const cardContainer = document.getElementById('card-container')
-const slideContainer = cardContainer.querySelector('[data-role="slider-container"]')
-const arrowsInSlideContainer = slideContainer.querySelectorAll('button')
+const switchContainer = cardContainer.querySelector('[data-role="switch-container"]')
+const slides = cardContainer.querySelectorAll('[data-role="slide"]')
+const arrowsInswitchContainer = switchContainer.querySelectorAll('button')
 const effectContainer = productContainer.querySelector('.effect-container')
 const transitionContainer = effectContainer.querySelectorAll('.transition-container')
 const buyButton = document.getElementById('buy-button')
 
-arrowsInSlideContainer.forEach(arrow => {
-  arrow.addEventListener('click', handleRoundedTransition)
+console.log(slides)
+
+arrowsInswitchContainer.forEach(arrow => {
+  arrow.addEventListener('click', event => handleSlide(event))
 })
 
 let index = 0
 
-function handleRoundedTransition() {
-  index++
+function handleSlide (event) {
+  const parentElement = event.target.parentNode
+
+  if (parentElement.id === "next-slide") {
+    index++
+  }
+
+  if (parentElement.id === "previous-slide") {
+    index--
+  }
 
   if (index >= transitionContainer.length) {
     index = 0
+  }
+
+  if (index < 0) {
+    index = transitionContainer.length - 1
+  }
+
+  for (let i = 0; i < transitionContainer.length; i++) {
+    if (i === index) {
+      transitionContainer[i].classList.add('-active')
+    } else {
+      transitionContainer[i].classList.remove('-active')
+    }
   }
 
   for (let i = 0; i < transitionContainer.length; i++) {
