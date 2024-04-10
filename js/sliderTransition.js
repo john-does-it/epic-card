887ad9cd5140de842446194995fad5f82d92560b
Jonathan Schoonbroodt <hello@johndoesit.be>
2024-04-10 02:13:55 +0200

    css + js  / animation

--- js/sliderTransition.js
+++ js/sliderTransition.js
@@ -1,10 +1,40 @@
-const cardTransitionContainer = document.getElementById('card-transition-container')
+const productContainer = document.getElementById('product-container')
+const cardContainer = document.getElementById('card-container')
+const slideContainer = cardContainer.querySelector('[data-role="slider-container"]')
+const arrowsInSlideContainer = slideContainer.querySelectorAll('button')
+const effectContainer = productContainer.querySelector('.effect-container')
+const transitionContainer = effectContainer.querySelectorAll('.transition-container')
+const buyButton = document.getElementById('buy-button')
 
-/* 
-  when clicking on the arrow: 
-  1. we apply the transition to the rounded container
-  2. we change the number in the currentslide span
-  3. we apply the background-color transition to the background
-  4. we apply the fade-out / fade-in transition to the chairs pictures
-*/
+arrowsInSlideContainer.forEach(arrow => {
+  arrow.addEventListener('click', handleRoundedTransition)
+})
+
+let index = 0
+
+function handleRoundedTransition() {
+  index++
+
+  if (index >= transitionContainer.length) {
+    index = 0
+  }
+
+  for (let i = 0; i < transitionContainer.length; i++) {
+    if (i === index) {
+      transitionContainer[i].classList.add('-active')
+    } else {
+      transitionContainer[i].classList.remove('-active')
+    }
+  }
+
+  setTimeout(() => {
+    const activeFillingColor = transitionContainer[index].querySelector('.fillingcolor')
+    const backgroundColor = window.getComputedStyle(activeFillingColor).backgroundColor
+    cardContainer.style.backgroundColor = backgroundColor
+  }, 500)
+}
+
+
+
+
 
